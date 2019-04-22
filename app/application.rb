82ae49e3a264f87ab13ffbe522c7dea77f23bb64
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-22 14:47:30 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,5 +4,20 @@ class Application
     resp = Rack::Response.new  
     req = Rack::Request.new(env) 
     
-     
+     if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.find {|i| i.name == item_name}
+        resp.write item.price
+      else 
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+    resp.finish
+    
+  end
+  
 end 
